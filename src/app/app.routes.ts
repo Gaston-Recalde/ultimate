ab87060073ef336db1c2c86dd83c8acb9b53903c
Gaston Recalde <gastonrg94@gmail.com>
2024-02-16 20:53:13 -0300

    Sistema de rutas bases creadas, no childrens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UltimateComponent } from './pages/ultimate/ultimate.component';
+import { LuffComponent } from './pages/luff/luff.component';
+import { HatsComponent } from './pages/hats/hats.component';
+import { NoticiasComponent } from './pages/noticias/noticias.component';
+import { EquiposComponent } from './pages/equipos/equipos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'ultimate', component: UltimateComponent },
+    { path: 'luff', component: LuffComponent },
+    { path: 'hats', component: HatsComponent },
+    { path: 'noticias', component: NoticiasComponent },
+    { path: 'equipos', component: EquiposComponent }
+];
