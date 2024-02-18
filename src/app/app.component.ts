e77a0c12f7b4bed7a9029108c42750fc05601e4f
Gaston Recalde <gastonrg94@gmail.com>
2024-02-17 21:38:01 -0300

    Sidenav y Toolbar basicos añadidos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    RouterLink,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatListModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ultimate';
+
+  public sidenarItems = [
+    { label: 'Ultimate', icon: 'label', url: './ultimate' },
+    { label: 'LUFF', icon: 'add', url: './luff' },
+    { label: 'Hats', icon: 'add', url: './hats' },
+    { label: 'Noticias', icon: 'search', url: './noticias' },
+    { label: 'Equipos', icon: 'search', url: './equipos' },
+  ]
 }
