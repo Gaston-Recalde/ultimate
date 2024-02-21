a0d314f8f84a1032247f03a36d3a352dd73df8ed
Gaston Recalde <gastonrg94@gmail.com>
2024-02-20 21:42:51 -0300

    Inicio de implementacion de Card en home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatCardModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
